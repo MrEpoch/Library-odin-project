50118f33618ee00e4b6f4e389ac1bc5619c3086f
MrEpoch <109413478+MrEpoch@users.noreply.github.com>
2023-01-11 15:06:24 +0100

    Gave querySelectors into the objects

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const addORdisplay = document.querySelector(".btn-add-new");
-const previusORreset = document.querySelector(".btn-display-previus");
-const nextORsubmit = document.querySelector(".btn-display-next");
-const book = document.querySelector(".book");
-const remove = document.querySelector(".rm");
+const LocationSelectors = {
+  addORdisplay: document.querySelector(".btn-add-new"),
+  previusORreset: document.querySelector(".btn-display-previus"),
+  nextORsubmit: document.querySelector(".btn-display-next"),
+  book: document.querySelector(".book"),
+  remove: document.querySelector(".rm"),
+};
 let current = 0;
 const myLibrary = [];
-const one = nextORsubmit.innerHTML;
 const objects = {
   pen: `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
           <path fill="currentColor" d="M14.1,9L15,9.9L5.9,19H5V18.1L14.1,9M17.7,3C17.5,3 17.2,3.1 17,3.3L15.2,5.1L18.9,8.9L20.7,7C21.1,6.6 21.1,6 20.7,5.6L18.4,3.3C18.2,3.1 17.9,3 17.7,3M14.1,6.2L3,17.2V21H6.8L17.8,9.9L14.1,6.2M7,2V5H10V7H7V10H5V7H2V5H5V2H7Z"></path>
@@ -22,31 +23,30 @@ const objects = {
   display: `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
             <path fill="currentColor" d="M13,12H20V13.5H13M13,9.5H20V11H13M13,14.5H20V16H13M21,4H3A2,2 0 0,0 1,6V19A2,2 0 0,0 3,21H21A2,2 0 0,0 23,19V6A2,2 0 0,0 21,4M21,19H12V6H21"></path>
           </svg>`,
-  submit: one,
+  submit: LocationSelectors.nextORsubmit.innerHTML,
 };
-
 
 const BuildingBlocks = () => {
   const emptyTags = () => {
-    book.innerHTML = `
+    LocationSelectors.book.innerHTML = `
     <h4 class="title">title: </h4>
     <h4 class="author">author: </h4>
     <h4 class="pages">pages: </h4>
     <h4 class="read">read: </h4>
     `;
-    return book;
-  }
+    return LocationSelectors.book;
+  };
   const fullLibrary = () => {
-    book.innerHTML = `
+    LocationSelectors.book.innerHTML = `
     <h4 class="title">title: ${myLibrary[current].title}</h4>
     <h4 class="author">author: ${myLibrary[current].author}</h4>
     <h4 class="pages">pages: ${myLibrary[current].pages}</h4>
     <h4 class="read">read: ${myLibrary[current].read}</h4>
     `;
-    return book;
-  }
+    return LocationSelectors.book;
+  };
   const write = () => {
-    book.innerHTML = `
+    LocationSelectors.book.innerHTML = `
     <div class="title">
       <label for="title">Title: </label>
       <input type="text" class="title-text" placeholder="Harry Potter">
@@ -64,11 +64,10 @@ const BuildingBlocks = () => {
       <input type="text" class="read-text">
     </div>
   `;
-    return book;
-  }
-  return { emptyTags, fullLibrary, write }
-}
-
+    return LocationSelectors.book;
+  };
+  return { emptyTags, fullLibrary, write };
+};
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -85,18 +84,18 @@ function addBook(titleAdd, authorAdd, pagesAdd, readAdd) {
   myLibrary[id] = new Book(titleAdd, authorAdd, pagesAdd, readAdd);
 }
 
-addORdisplay.addEventListener("click", () => {
-  if (addORdisplay.innerHTML === objects.pen) {
-    addORdisplay.innerHTML = objects.display;
-    nextORsubmit.innerHTML = objects.submit;
-    previusORreset.innerHTML = objects.reset;
-    remove.innerHTML = "";
+LocationSelectors.addORdisplay.addEventListener("click", () => {
+  if (LocationSelectors.addORdisplay.innerHTML === objects.pen) {
+    LocationSelectors.addORdisplay.innerHTML = objects.display;
+    LocationSelectors.nextORsubmit.innerHTML = objects.submit;
+    LocationSelectors.previusORreset.innerHTML = objects.reset;
+    LocationSelectors.remove.innerHTML = "";
     BuildingBlocks().write();
   } else {
-    addORdisplay.innerHTML = objects.pen;
-    nextORsubmit.innerHTML = objects.next;
-    previusORreset.innerHTML = objects.prev;
-    remove.innerHTML = `<button type="button" class="btn-remove">REMOVE BOOK</button>`;
+    LocationSelectors.addORdisplay.innerHTML = objects.pen;
+    LocationSelectors.nextORsubmit.innerHTML = objects.next;
+    LocationSelectors.previusORreset.innerHTML = objects.prev;
+    LocationSelectors.remove.innerHTML = `<button type="button" class="btn-remove">REMOVE BOOK</button>`;
     if (!myLibrary[current]) {
       BuildingBlocks().emptyTags();
     } else {
@@ -117,19 +116,19 @@ addORdisplay.addEventListener("click", () => {
   }
 });
 
-nextORsubmit.addEventListener("click", () => {
+LocationSelectors.nextORsubmit.addEventListener("click", () => {
   const title = document.querySelector(".title-text");
   const author = document.querySelector(".author-text");
   const pages = document.querySelector(".pages-number");
   const read = document.querySelector(".read-text");
-  if (nextORsubmit.innerHTML === objects.submit) {
+  if (LocationSelectors.nextORsubmit.innerHTML === objects.submit) {
     addBook(title.value, author.value, pages.value, read.value);
     title.value = "";
     author.value = "";
     pages.value = "";
     read.value = "";
   } else if (
-    nextORsubmit.innerHTML === objects.next &&
+    LocationSelectors.nextORsubmit.innerHTML === objects.next &&
     myLibrary.length !== 0
   ) {
     const none = 0;
@@ -142,18 +141,18 @@ nextORsubmit.addEventListener("click", () => {
   }
 });
 
-previusORreset.addEventListener("click", () => {
+LocationSelectors.previusORreset.addEventListener("click", () => {
   const title = document.querySelector(".title-text");
   const author = document.querySelector(".author-text");
   const pages = document.querySelector(".pages-number");
   const read = document.querySelector(".read-text");
-  if (previusORreset.innerHTML === objects.reset) {
+  if (LocationSelectors.previusORreset.innerHTML === objects.reset) {
     title.value = "";
     author.value = "";
     pages.value = "";
     read.value = "";
   } else if (
-    previusORreset.innerHTML === objects.prev &&
+    LocationSelectors.previusORreset.innerHTML === objects.prev &&
     myLibrary.length !== 0
   ) {
     const none = 0;
